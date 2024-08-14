Fixed the instable gradients by clamping values in the simulator

dynaphos/simulator.py
@@ -145,7 +145,10 @@ class Sigma(State):
         p = self.params['size']
         if p['size_equation'] == 'sqrt':  # Tehovnik 2007
             def f(x):
-                return torch.sqrt(torch.div(x, p['current_spread']))
+                division_result = torch.div(x, p['current_spread'])
+                # Clamp values to avoid division by zero during backprop
+                division_result = torch.clamp(division_result, min=1e-8)
+                return torch.sqrt(division_result)
         elif p['size_equation'] == 'sigmoid':  # Bosking et al., 2017
             def f(x):
                 return 0.5 * p['MD'] * sigmoid(p['slope_size'] *
@@ -365,8 +368,13 @@ class GaussianSimulator:
         """
 
         # Calculate normalized Gaussian (peak has value 1).
-        sigma = self.sigma.get().clamp(1e-22, None)  # TODO: clamping redundant? Default division by zero gives inf.
-        exp = torch.exp(-0.5 * (self.phosphene_maps / sigma) ** 2)
+        sigma = self.sigma.get().clamp(min=1e-6, max=1e6)
+        # Clamp exponent
+        exponent = -0.5 * (self.phosphene_maps / sigma) ** 2
+        exponent = torch.clamp(exponent, min=-20, max=20)
+        exp = torch.exp(exponent)
+        # Clamp the output of the exponential function to ensure it doesn't fall below 1e-8
+        exp = torch.clamp(exp, min=1e-8)
         return exp
 
     def get_state(self):

model.py
@@ -64,7 +64,6 @@ class MiniConvNet(nn.Module):
         theta = self.conv_padding(xs)
         theta = torch.clamp(theta, min=theta.mean(), max=theta.max())
         theta = torch.sigmoid(theta)
-
         return theta
 
 
@@ -85,16 +84,13 @@ class PhospheneOptimizer(nn.Module):
         self.get_learnable_params.load_state_dict(self.init_weights, strict=False)
         self.extract_saliency_map = SaliencyMap(self.model_params, requires_grad=True)
 
-        # self.clip_model, self.preprocess = clip.load(self.model_params.saliency_clip_model, device=self.model_params.device,
-        #                                              jit=False)
-        # self.clip_model.eval().to(self.model_params.device)
-
-    def forward(self, input_image): #clip_attention_map, saliency_map_soft --> attention_map, saliency_map
+    def forward(self, input_image):
         clip_attention_map, saliency_map = self.extract_saliency_map(input_image)
         phosphene_placement_map = self.get_learnable_params(saliency_map.unsqueeze(0).unsqueeze(0))
+        # Rescale pixel intensities between [0, <max_stimulation_intensity_ampere>]
+        phosphene_placement_map = self.normalized_rescaling(phosphene_placement_map, max_stimulation_intensity=self.simulator_params['sampling']['stimulus_scale'])
         # Make the phosphene_placement_map as a stimulation vector for the phosphene simulator
-        phosphene_placement_map = self.normalized_rescaling(phosphene_placement_map)
-        phosphene_placement_map = self.simulator.sample_stimulus(phosphene_placement_map, rescale=False)
+        phosphene_placement_map = self.simulator.sample_stimulus(phosphene_placement_map)
         self.simulator.reset()
         optimized_im = self.simulator(phosphene_placement_map)
         optimized_im = optimized_im.unsqueeze(0)
@@ -103,72 +99,12 @@ class PhospheneOptimizer(nn.Module):
         del clip_attention_map, saliency_map
         return optimized_im
 
-    def normalized_rescaling(self, phosphene_placement_map):  # 100e-6
+    def normalized_rescaling(self, phosphene_placement_map, max_stimulation_intensity=1):  # 100e-6
         """Normalize <img> and rescale the pixel intensities in the range [0, <stimulus_scale>].
+        <stimulus_scale> is defined in the parameter file.
         The output image represents the stimulation intensity map.
-        param stimulus_scale: the stimulation amplitude corresponding to the highest-valued pixel.
         return: image with rescaled pixel values (stimulation intensity map in Ampères)."""
 
         img_norm = (phosphene_placement_map - phosphene_placement_map.min()) / (phosphene_placement_map.max() - phosphene_placement_map.min())
-        return img_norm * self.simulator_params['sampling']['stimulus_scale']
+        return img_norm * max_stimulation_intensity
 
-
-#     def get_clip_saliency_map(self, args, target_im):
-#
-#         data_transforms = transforms.Compose([
-#             self.preprocess.transforms[-1],
-#         ])
-#         target_im[target_im == 0.] = 1.
-#         image_input_clip = data_transforms(target_im).to(args.device)
-#         text_input_clip = clip.tokenize([self.model_params.text_target]).to(args.device)
-#
-#         attention_map = interpret(image_input_clip, text_input_clip, self.clip_model,
-#                                   device=args.device)
-#
-#         attn_map = (attention_map - attention_map.min()) / (attention_map.max() - attention_map.min())
-#
-#         xdog = clipasso_model.XDoG_()
-#         im_xdog = xdog(image_input_clip[0].permute(1, 2, 0).cpu().numpy(), k=10)
-#         intersec_map = (1 - im_xdog) * attn_map  # Multiplication of attention map and edge map
-#
-#         clip_saliency_map = np.copy(intersec_map)
-#         clip_saliency_map[intersec_map > 0] = self.softmax(intersec_map[intersec_map > 0],
-#                                                       tau=args.softmax_temp)
-#         clip_saliency_map = torch.Tensor(clip_saliency_map) / clip_saliency_map.max()
-#         clip_saliency_map.requires_grad = True
-#
-#         return attention_map, clip_saliency_map
-#
-#     def softmax(self, x, tau=0.2):
-#         e_x = np.exp(x / tau)
-#         return e_x / e_x.sum()
-#
-# def interpret(image, texts, model, device):
-#     images = image.repeat(1, 1, 1, 1)
-#     res = model.encode_image(images)
-#     model.zero_grad()
-#     image_attn_blocks = list(dict(model.visual.transformer.resblocks.named_children()).values())
-#     num_tokens = image_attn_blocks[0].attn_probs.shape[-1]
-#     R = torch.eye(num_tokens, num_tokens, dtype=image_attn_blocks[0].attn_probs.dtype).to(device)
-#     R = R.unsqueeze(0).expand(1, num_tokens, num_tokens)
-#     cams = []  # there are 12 attention blocks
-#     for i, blk in enumerate(image_attn_blocks):
-#         cam = blk.attn_probs.detach()
-#         # each patch is 7x7, so we have 49 pixels + 1 for positional encoding
-#         cam = cam.reshape(1, -1, cam.shape[-1], cam.shape[-1])
-#         cam = cam.clamp(min=0)
-#         cam = cam.clamp(min=0).mean(dim=1)
-#         cams.append(cam)
-#         R = R + torch.bmm(cam, R)
-#
-#     cams_avg = torch.cat(cams)
-#     cams_avg = cams_avg[:, 0, 1:]
-#     image_relevance = cams_avg.mean(dim=0).unsqueeze(0)
-#     image_relevance = image_relevance.reshape(1, 1, 7, 7)
-#     image_relevance = torch.nn.functional.interpolate(image_relevance, size=224, mode='bicubic')
-#     image_relevance = image_relevance.reshape(224, 224).data.cpu().numpy().astype(np.float32)
-#     image_relevance = (image_relevance - image_relevance.min()) / (image_relevance.max() - image_relevance.min())
-#     return image_relevance
-#
-#
-#

training_model.py
@@ -4,7 +4,8 @@ import sys
 
 import numpy as np
 from torchvision import transforms
-from torch.utils.data import DataLoader, Dataset
+import torch.nn.utils as utils
+from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 from tqdm.auto import tqdm
 
@@ -15,6 +16,7 @@ from model import PhospheneOptimizer
 from clipasso.models.loss import Loss
 from clipasso import painterly_rendering
 
+torch.autograd.set_detect_anomaly(True)
 
 def main(model_params):
 
@@ -34,20 +36,26 @@ def main(model_params):
     else:
         epoch_range = tqdm(range(model_params.num_iter))
     for epoch in epoch_range:
+        counter = 0
         for batch in train_loader:
+            if counter > 0:
+                break
             losses = []
             batch_losses = []
             # we loop over samples in the batch as clipasso expect only one sample as input
-            for sample in batch:
+            images = batch[0]
+            for sample in images:
                 # Data preprocessing and augmentation performed by clipasso from PIL images
                 input_img = to_pil(sample.squeeze(0))
                 input_img, mask = clipasso.painterly_rendering.get_target(model_params, input_img)
+                # input_img[input_img == 0.] = 1. # It was because of Xdog which would produce weird saliency maps
                 input_img = input_img.to(model_params.device)
                 optimizer.zero_grad()
                 output_img = model(input_img)
-                # output_img[output_img == 1.] = 0.  # Make image background black
+                # output_img[output_img == 1.] = 0.  # Turn it back to black
                 losses_dict = loss_func(output_img, input_img, model.parameters(), epoch, optimizer)
                 losses.append(sum(losses_dict.values()))
+            counter =+ 1
             # Average loss over samples in the batch
             loss = sum(losses) / len(losses)
             loss.backward()