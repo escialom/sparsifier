9a9b6d12419cdca47619d6b4eea4fb7e5597cfe5
escialom <elsa.scialom@epfl.ch>
2024-08-15 11:22:03 +0200

    Got rid of the batch loop

--- training_model.py
+++ training_model.py
@@ -3,9 +3,6 @@ import traceback
 import sys
 
 import numpy as np
-from torchvision import transforms
-import torch.nn.utils as utils
-from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 from tqdm.auto import tqdm
 
@@ -26,46 +23,37 @@ def main(model_params):
     model.train()
     optimizer = torch.optim.Adam(model.parameters(), lr=model_params.lr)
     loss_func = Loss(model_params)
-    train_dataset = ImageFolder(root=model_params.train_set, transform=transforms.ToTensor())
-    train_loader = DataLoader(train_dataset, batch_size=model_params.batch_size, shuffle=True)
-    to_pil = transforms.ToPILImage()
+    train_dataset = ImageFolder(root=model_params.train_set)
 
     epoch_loss = []
     if model_params.display:
         epoch_range = range(model_params.num_iter)
     else:
         epoch_range = tqdm(range(model_params.num_iter))
+
     for epoch in epoch_range:
-        counter = 0
-        for batch in train_loader:
-            if counter > 0:
-                break
-            losses = []
-            batch_losses = []
-            # we loop over samples in the batch as clipasso expect only one sample as input
-            images = batch[0]
-            for sample in images:
-                # Data preprocessing and augmentation performed by clipasso from PIL images
-                input_img = to_pil(sample.squeeze(0))
-                input_img, mask = clipasso.painterly_rendering.get_target(model_params, input_img)
-                # input_img[input_img == 0.] = 1. # It was because of Xdog which would produce weird saliency maps
-                input_img = input_img.to(model_params.device)
-                optimizer.zero_grad()
-                output_img = model(input_img)
-                # output_img[output_img == 1.] = 0.  # Turn it back to black
-                losses_dict = loss_func(output_img, input_img, model.parameters(), epoch, optimizer)
-                losses.append(sum(losses_dict.values()))
-            counter =+ 1
-            # Average loss over samples in the batch
-            loss = sum(losses) / len(losses)
+        sample_losses = []
+
+        for sample in train_dataset:
+            input_img, _ = sample
+            # Data preprocessing and augmentation performed by clipasso from PIL images
+            input_img, mask = clipasso.painterly_rendering.get_target(model_params, input_img)
+            input_img = input_img.to(model_params.device)
+            optimizer.zero_grad()
+            output_img = model(input_img)
+            losses_dict = loss_func(output_img, input_img, model.parameters(), epoch, optimizer)
+            loss = sum(losses_dict.values())
             loss.backward()
             optimizer.step()
-            batch_losses.append(loss.item())
-        # Save loss of each epoch
+            sample_losses.append(loss.item())
+
+        # Average loss over the epoch
+        avg_epoch_loss = sum(sample_losses) / len(sample_losses)
         epoch_loss.append({
             "epoch": epoch + 1,
-            "epoch_loss": sum(batch_losses) / len(batch_losses)
+            "epoch_loss": avg_epoch_loss
         })
+
     return epoch_loss
 
 
